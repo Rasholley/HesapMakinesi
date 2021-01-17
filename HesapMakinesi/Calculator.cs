9bced70787be20df19717957d63ba44acfe7671d
SARIKOCA <SARIKOCA@SARKOCA>
2021-01-17 13:56:14 +0300

    Proje dosyası ekle.

---
+++ HesapMakinesi/Calculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HesapMakinesi
+{
+    class Calculator
+    {
+        public int Number1 { get; set; }
+        public int Number2 { get; set; }
+        public void Topla(int a , int b)
+        {
+            int toplam = a + b;
+            Console.WriteLine($"Toplam = {toplam}");
+        }
+        public void Cikar(int a, int b)
+        {
+            int cikarim = a - b;
+            Console.WriteLine($"Toplam = {cikarim}");
+        }
+        public void Carpma(int a, int b)
+        {
+            int carpim = a * b;
+            Console.WriteLine($"Carpim = {carpim}");
+        }
+        public void Bolme(int a , int b)
+        {
+            double bolum = a / b;
+            Console.WriteLine($"Bolum = {bolum}");
+        }
+        public void Faktoriyel(int a)
+        {
+            int Faktoriyel = 1;
+            for (int i = 1; i <= a; i++)
+            {
+               Faktoriyel = Faktoriyel * i;
+            }
+            Console.WriteLine($"Faktoriyel = {Faktoriyel}");
+        }
+       
+    }
+}
---
+++ HesapMakinesi/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace HesapMakinesi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------------- Hesap Makinesi --------------");
+            int x = 5;
+            Calculator calculator = new Calculator();
+            while (x == 5)
+            {
+                Console.WriteLine("Toplama İşlemi (1)" +
+                "Çıkarma Işlemi (2)" +
+                "Çarpma Işlemi (3)" +
+                "Bölme Işlemi (4)" +
+                "Faktoriyel Alma (5)");
+                Console.WriteLine("Sayi Giriniz :");
+                int select = Convert.ToInt32(Console.ReadLine());
+
+                if (select == 1)
+                {
+                    Console.Write("1. Sayiyi Giriniz");
+                    int number1 = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("2. Sayiyi Giriniz");
+                    int number2 = Convert.ToInt32(Console.ReadLine());
+                    calculator.Topla(number1, number2);
+                }
+                else if (select == 2)
+                {
+                    Console.Write("1. Sayiyi Giriniz");
+                    int number1 = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("2. Sayiyi Giriniz");
+                    int number2 = Convert.ToInt32(Console.ReadLine());
+                    calculator.Cikar(number1, number2);
+                }
+                else if (select == 3)
+                {
+                    Console.Write("1. Sayiyi Giriniz");
+                    int number1 = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("2. Sayiyi Giriniz");
+                    int number2 = Convert.ToInt32(Console.ReadLine());
+                    calculator.Carpma(number1, number2);
+                }
+                else if (select == 4)
+                {
+                    Console.Write("1. Sayiyi Giriniz");
+                    int number1 = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("2. Sayiyi Giriniz");
+                    int number2 = Convert.ToInt32(Console.ReadLine());
+                    calculator.Bolme(number1, number2);
+                }
+                else if (select == 5)
+                {
+                    Console.Write("Faktoriyelini Almak Istediginiz Sayiyi Giriniz : ");
+                    int number1 = Convert.ToInt32(Console.ReadLine());
+                    calculator.Faktoriyel(number1);
+                }
+
+            }
+            
+            
+
+        }
+    }
+}
